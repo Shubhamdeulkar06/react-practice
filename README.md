b0fea090f0427559777623588ce1ce8b97e850e7
Shubhamdeulkar06 <shuboy0868@gmail.com>
2024-02-12 22:37:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React day 1
+
+- HOT MODULE RELOADING
+- File watcher algorithm
+- Bundling
+- Minified code
+- Image optimization
+- Port
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-3xl font-bold underline bg-red-400 text-center">
+      Hi Mamu
+    </div>
+  );
+}
+
+export default App;
